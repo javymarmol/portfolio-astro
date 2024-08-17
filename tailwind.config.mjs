808936894bc4d7a38075d81c178dcb1f0e6854bb
Heyner Javier Marmol <contacto@javymarmol.com>
2024-08-17 01:27:09 -0500

    added cards to layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,19 @@
+import defaultTheme  from 'tailwindcss/defaultTheme'
+import {addIconSelectors} from "@iconify/tailwind";
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			extend: {
+				fontFamily: {
+					sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+					serif: ['Figtree', ...defaultTheme.fontFamily.serif],
+				},
+			},
+		},
 	},
-	plugins: [],
+	plugins: [
+		addIconSelectors(['ri']),
+	],
 }
