fc969a84beccf5e08ffb1f242c2f0b947e3fde51
Heyner Javier Marmol <contacto@javymarmol.com>
2024-09-04 06:56:16 -0500

    generate sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,12 @@ import tailwind from "@astrojs/tailwind";
 
 import vue from "@astrojs/vue";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue()],
+  integrations: [tailwind(), vue(), sitemap({
+    customPages: ['https://blog.javymarmol.com'],
+  })],
   site: "https://javymarmol.com",
 });
