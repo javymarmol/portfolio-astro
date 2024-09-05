fe0bc761a54dfea986d64a3b5312ed1afbfe5d96
Heyner Javier Marmol <contacto@javymarmol.com>
2024-09-05 02:16:44 -0500

    optimization for lighthouse rank

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,11 +5,8 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
-			extend: {
-				fontFamily: {
-					sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-					serif: ['Figtree', ...defaultTheme.fontFamily.serif],
-				},
+			fontFamily: {
+				sans: ["'Figtree Variable'", ...defaultTheme.fontFamily.sans],
 			},
 		},
 	},
